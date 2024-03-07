74d80fb4fb1c88d3352387ffb7f74f52ce5a49b7
hoonisfree <118662392+hoonisfree@users.noreply.github.com>
2024-03-07 16:06:55 +0900

    [level 2] Title: 주식가격, Time: 21.34 ms, Memory: 61.2 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,15 +1,23 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] prices) {
-        int len = prices.length;
-        int[] answer = new int[len];
-        int i, j;
-        for (i = 0; i < len; i++) {
-            for (j = i + 1; j < len; j++) {
-                answer[i]++;
-                if (prices[i] > prices[j])
-                    break;
+        int[] answer = new int[prices.length];
+        Stack<Integer> stack = new Stack<>();
+        
+        for (int i = 0; i < prices.length; i++) {
+            while (!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
+                int index = stack.pop();
+                answer[index] = i - index;
             }
+            stack.push(i);
         }
+        
+        while (!stack.isEmpty()) {
+            int index = stack.pop();
+            answer[index] = prices.length - index - 1;
+        }
+        
         return answer;
     }
 }
