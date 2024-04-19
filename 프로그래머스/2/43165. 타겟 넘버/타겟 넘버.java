64c9f0842caa2de39f8ddc8109fe939dedbc861b
hoonisfree <118662392+hoonisfree@users.noreply.github.com>
2024-04-19 16:01:24 +0900

    [level 2] Title: 타겟 넘버, Time: 0.45 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,18 +1,6 @@
 class Solution {
     public int solution(int[] numbers, int target) {
-        return dfs(numbers,target,0,0);
-    }
-    public int dfs(int[] numbers, int target, int i, int sum){
-        if(i==numbers.length){
-            if(sum == target){
-                return 1;
-            }else{
-                return 0;
-            }
-        }
-        int plus = dfs(numbers,target,i+1,sum+numbers[i]);
-        int minus = dfs(numbers,target,i+1,sum-numbers[i]);
-        
-    return plus+minus;
+        int answer = 0;
+        return answer;
     }
 }
