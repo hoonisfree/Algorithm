5b07604726f8942bbfeccd03bccf70295b7a62be
hoonisfree <118662392+hoonisfree@users.noreply.github.com>
2024-06-13 18:18:11 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.12 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -5,17 +5,16 @@ class Solution {
         
         for(int i = 0; i < words.length; i++){
             String word = words[i];
-            StringBuilder change = new StringBuilder();
+        
             
             for (int j = 0; j < word.length(); j++) {
                 char c = word.charAt(j);
                 if (j % 2 == 0) {
-                    change.append(Character.toUpperCase(c));
+                    result.append(Character.toUpperCase(c));
                 } else {
-                    change.append(Character.toLowerCase(c));
+                    result.append(Character.toLowerCase(c));
                 }
             }
-            result.append(change);
             
             if (i < words.length - 1) {
                 result.append(" ");
